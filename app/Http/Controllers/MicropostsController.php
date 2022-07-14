3764bcc949d92e46542354e74a1b2f29135d9381
shinjirou.iwata <sin2021@outlook.jp>
2022-07-14 23:34:50 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -10,12 +10,12 @@ class MicropostsController extends Controller
      public function index()
     {
         $data = [];
-        if (\Auth::check()) { // 認証済みの場合
+        if (\Auth::check()) { 
             // 認証済みユーザを取得
             $user = \Auth::user();
-            // ユーザの投稿の一覧を作成日時の降順で取得
-            // （後のChapterで他ユーザの投稿も取得するように変更しますが、現時点ではこのユーザの投稿のみ取得します）
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+        
+            // ユーザとフォロー中のユーザの投稿の一覧を作成日時の降順で取得
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
@@ -26,6 +26,7 @@ class MicropostsController extends Controller
         // Welcomeビューでそれらを表示
         return view('welcome', $data);
     }
+    
      public function store(Request $request)
     {
         // バリデーション
