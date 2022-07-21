d0cd0b5be4eeb188b94b484f06b057be46f80955
shinjirou.iwata <sin2021@outlook.jp>
2022-07-21 00:05:47 +0000

    favorite change

--- resources/views/favorites/favoriteBtn.blade.php
+++ resources/views/favorites/favoriteBtn.blade.php
@@ -1,11 +1,11 @@
     @if(Auth::user()->is_favorite($micropost->id))
         {{-- いいねボタン外すのフォーム --}}
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-sm btn-success"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-sm btn-primary"]) !!}
         {!! Form::close() !!}
     @else
         {{-- いいねするボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-sm btn-primary"]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-sm btn-success"]) !!}
         {!! Form::close() !!}
     @endif
